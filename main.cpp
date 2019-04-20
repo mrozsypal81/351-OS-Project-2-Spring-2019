1f77102ea9a8a1032f0f46c1542cb4ab355a8c34
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2019-04-19 20:44:10 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "process.h"   
 #include <iostream>
+//#include <iomanip>
 #include <fstream>
 #include <string>
+#include <queue>
 
 using namespace std;
 int main() {
@@ -10,20 +12,29 @@ int main() {
 	int Page_Size = -1;
 	int num_of_processes = -1;
 	process processes[20]; // at most 20 processes
-
+	int clock = 0;		// timer start at 0
+	queue<string> waitList = {};
+	bool memory_map = false;
+	bool report_clock = false;
+	int memory_space = 0;
+	string map[300000] =  {"Free frame(s)"};
+	
 //-------------------------Initialize Memory---------------------------
 	bool correct_memory = true;
 	do {
-		cout << "Please enter memory size: ";
+		cout << "Please enter memory size(0-30000): ";
 		cin >> Memory_Size;
 		cout << "Please enter page size: ";
 		cin >> Page_Size;
-		correct_memory = (Memory_Size >= Page_Size) ? true : false;
-		if (correct_memory == false) {
+		if (Memory_Size < 0 || Page_Size < 0 || Memory_Size <= Page_Size) {
+			correct_memory = false;
 			cout << "ERROR!!! "
-				<< "Invalid input, page size must smaller than memory size, please enter again." << endl;
+				 << "Invalid input!!! "
+				 << "Please enter again." 
+				 << endl;
 		}
 	} while (!correct_memory);
+	
 //-------------------------read file-----------------------------------
 	//get file name
 	string fileName;
@@ -43,7 +54,7 @@ int main() {
 			string id = "";
 			myFile >> id;
 			processes[i].set_id(id);
-			//cout << id << endl;
+			//cout << processes[i].get_id() << endl;
 
 			//set arriving time and burst time
 			int arriving_time = -1;
@@ -51,7 +62,7 @@ int main() {
 			myFile >> arriving_time >> burst_time;
 			processes[i].set_arriving_time(arriving_time);
 			processes[i].set_burst_time(burst_time);
-			//cout << arriving_time << " " << burst_time << endl;
+			//cout << processes[i].get_arriving_time() << " " << processes[i].get_burst_time() << endl;
 
 			//set memory size
 			int memory_request_num = 0;
@@ -63,11 +74,50 @@ int main() {
 				sum += memory_request_size[j];
 			}
 			processes[i].set_memory_request_size(sum);
-			//cout << memory_request_num << " " << sum << endl;
+			//cout << memory_request_num << " " << processes[i].get_memory_request_size() << endl;
 
 		}
 	}
+
+	//close file
 	myFile.close();
+
+//--------------------------clock running--------------------------------
+	while (clock != 100000) {
+	//----------------------waiting queue--------------------------------
+		for (int i = 0; i < num_of_processes; i++) {
+			//-----------------------add wait list-------------------------------
+			if (clock == processes[i].get_arriving_time()) {
+				waitList.push(processes[i].get_id());
+				if (!report_clock) {
+					cout << "t = " << clock << ": ";
+					report_clock = true;
+				}
+				cout << "Process " << processes[i].get_id() << " arrives" << endl;
+				cout << "Input Queue: ";
+				printQueue(waitList);
+			}
+		}
+	//-----------------------memory manager----------------------------------
+		if (!waitList.empty()) {
+			for (int i = 0; i < num_of_processes; i++) {
+				if (waitList.front() == processes[i].get_id()) {
+					cout << "MM moves Process " << waitList.front() << " to memory" << endl
+						<< "Input Queue: ";
+					waitList.pop();
+					printQueue(waitList);
+					memoryManager(processes[i], Memory_Size,Page_Size, memory_space, map);
+				}
+			}
+		}
+		
+
+
+		clock++;
+		report_clock = false;
+	}
+
+
 	system("pause");
 	return 0;
 	
--- process.h
+++ process.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -13,9 +14,24 @@ private:
 		int memory_request_size;
 
 public:
+//-------------------set data-------------------------------------
 	void set_id(string);
 	void set_arriving_time(int);
 	void set_burst_time(int);
-	void set_memory_request_num(int);
+	//void set_memory_request_num(int);
 	void set_memory_request_size(int);
+
+//-------------------get data--------------------------------------
+	string get_id();
+	int get_arriving_time();
+	int get_burst_time();
+	//int get_memory_request_num();
+	int get_memory_request_size();
+
 };
+
+
+
+//--------------------------main function---------------------
+void printQueue(queue<string>);
+void memoryManager(process, int, int, int, string[]);
