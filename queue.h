fa6d66827e451459aa258d8b13ac96f2195fc469
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2019-04-24 16:36:03 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,14 +1,22 @@
 #pragma once
+#include <vector>
 struct proc_queue {
 	int capacity;
 	int size;
 	int front;
 	int rear;
-	PROCESS **elements;
+	vector<PROCESS> elements;
 };
 
-proc_queue* create_proc_queue(int length) {
+proc_queue create_proc_queue(int length) {
+	proc_queue q;
+	q.elements.resize(length);
+	q.size = 0;
+	q.capacity = length;
+	q.front = 0;
+	q.rear = -1;
 
+	return q;
 }
 
 void enqueue_proc(proc_queue* q, PROCESS* proc) {
