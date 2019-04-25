9799ff1e73beb7947366f2c3d768a4e414e16b08
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2019-04-24 20:24:39 -0700

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <vector>
+
+using namespace std;
+
 struct proc_queue {
 	int capacity;
 	int size;
@@ -19,34 +22,55 @@ proc_queue create_proc_queue(int length) {
 	return q;
 }
 
-void enqueue_proc(proc_queue* q, PROCESS* proc) {
+proc_queue enqueue_proc(proc_queue q, PROCESS proc) {
+	if (q.size == q.capacity) {
+		cout << "ERROR: queue is full to capacity!" << endl;
+		exit(2);
+	}
 
+	q.size++;
+	q.rear++;
+
+	if (q.rear == q.capacity) {
+		q.rear = 0;
+	}
+	q.elements[q.rear] = proc;
+	return q;
 }
+
+PROCESS peek_queue_at_index(proc_queue q, int index) {
+	return q.elements[index];
+}
+
+int iterate_queue_index(proc_queue q, int index) {
+	return q.front + index;
+}
+
+void print_proc_queue(proc_queue q) {
+	PROCESS proc;
+
+	cout << "\tInput queue: [ ";
+	for (int i = 0; i < q.size; i ++) {
+		proc = peek_queue_at_index(q, iterate_queue_index(q, i));
+		cout << proc.pid << " ";
+	}
+	cout << "]" << endl;
+}
+
+
 
 int queue_has_next(proc_queue* q) {
 
 }
 
-PROCESS* peek_queue(proc_queue* q) {
-
-}
-
-PROCESS* peek_queue_at_index(proc_queue* q, int index) {
-	return q->elements[index];
-}
 
 void dequeue_proc(proc_queue* q) {
 
 }
 
-int iterate_queue_index(proc_queue* q, int index) {
-	return q->front + index;
-}
+
 
 void dequeue_proc_at_index(proc_queue* q, int index) {
 
 }
 
-void print_proc_queue(proc_queue* q) {
-
-}
